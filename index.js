477dd497029b4ad7c5b464e10e319920929a9e3e
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-04 14:57:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+// Load the DOM content.
+document.addEventListener("DOMContentLoaded", () => {
+    // Change the text on the page with an ID of text.
+  document.getElementById('text'). textContent = 'This is really cool!';
+})
